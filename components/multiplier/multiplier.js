e4866c38d044f4ad8383f47285918772aaea1a40
chaitanyapopuri <54385244+chaitanyapopuri@users.noreply.github.com>
2019-10-11 11:37:47 -0500

    fds

--- components/multiplier/multiplier.js
+++ components/multiplier/multiplier.js
@@ -1,7 +1,7 @@
-const add = (x, y) => { return x * y }
+const mul = (x, y) => { return x * y }
 
-const adder = new Vue({
-  el: '#adder',
+const multipler = new Vue({
+  el: '#multipler',
   data: {
     guest: 'Emmett',
     firstNumber: 5,
@@ -11,7 +11,7 @@ const adder = new Vue({
     result: function () {
       const i = parseInt(this.firstNumber)
       const j = parseInt(this.secondNumber)
-      return `${this.guest}, your sum is ${add(i, j)}.`
+      return `${this.guest}, your sum is ${mul(i, j)}.`
     }
   }
 })
